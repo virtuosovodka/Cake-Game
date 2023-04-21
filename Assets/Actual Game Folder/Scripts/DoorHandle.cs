4b12ce20b1a81a9c28b8545abb8e8835f04f63a5
Vedika Sheth <vedikasheth.18@gmail.com>
2023-04-21 10:33:23 -0700

    doordoordoor

---
+++ Assets/Actual Game Folder/Scripts/DoorHandle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DoorHandle : MonoBehaviour
+{
+    public TextMeshProUGUI debug;
+    public Player leftHand;
+    public Player rightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       // if (leftHand.ovenDoorHit.getName().CompareTag("OvenDoorHandle"))
+       // {
+            if (OVRInput.Get(OVRInput.RawButton.LHandTrigger)){
+
+            }
+        //}
+    }
+}
