0dd40e262c5b016d6e77fde695bcf18d51ec4cb8
LilyCrow <118573194+LilyCrow@users.noreply.github.com>
2023-05-03 15:34:15 -0700

    cake goes from tin to plate

---
+++ Assets/Actual Game Folder/Scripts/DetectCake.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCake : MonoBehaviour
+{
+    public GameObject cake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+      if (collision.gameObject.CompareTag("Cake"))
+        {
+            ConveyorBelt cb = GetComponent<ConveyorBelt>();
+            //cb.
+            //cb.enabled = true;
+        }
+    }
+
+}
