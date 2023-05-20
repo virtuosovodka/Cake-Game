befaf662577cb982bd5db17b6e72f5603363969f
Vedika Sheth <vedikasheth.18@gmail.com>
2023-05-19 17:54:29 -0700

    oven possibly works

--- Assets/Actual Game Folder/Scripts/OvenStuff.cs
+++ Assets/Actual Game Folder/Scripts/OvenStuff.cs
@@ -20,67 +20,76 @@ public class OvenStuff : MonoBehaviour
         gm.batter.SetActive(true);
 
         baked = false;
+
+        gm.timeInOven = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((gm.ovenOn || Input.GetKey(KeyCode.B)) && !baked)
+        if ((gm.ovenOn || Input.GetKey(KeyCode.B)))
         {
-            baked = true;
-
-            //deciding size of the cake that is being baked
-            //deciding color of the cake that is being baked depending on what color the batter chosen is
-
-            //mat = gm.batter.GetComponent<Renderer>().material;
-
-
-
-            gm.cake = gm.batterAmount < 1.5 ? gm.underfilled : gm.average;
-
-            Transform[] allChildren = gm.cake.GetComponentsInChildren<Transform>();
-
-            Debug.Log(allChildren.Length);
-
-            for (int i = 1; i < allChildren.Length; i++)
+            gm.timeInOven += Time.deltaTime;
+            if (!baked)
             {
-                allChildren[i].gameObject.GetComponent<Renderer>().material = gm.batter.GetComponent<Renderer>().material;
+                baked = true;
+
+                //deciding size of the cake that is being baked
+                //deciding color of the cake that is being baked depending on what color the batter chosen is
+
+                //mat = gm.batter.GetComponent<Renderer>().material;
+
+
+
+                gm.cake = gm.batterAmount < 1.5 ? gm.underfilled : gm.average;
+
+                Transform[] allChildren = gm.cake.GetComponentsInChildren<Transform>();
+
+                Debug.Log(allChildren.Length);
+
+                for (int i = 1; i < allChildren.Length; i++)
+                {
+                    allChildren[i].gameObject.GetComponent<Renderer>().material = gm.batter.GetComponent<Renderer>().material;
+                }
+
+                gm.cake.SetActive(true);
+                gm.batter.SetActive(false);
             }
 
-            gm.cake.SetActive(true);
-            gm.batter.SetActive(false);
-
-
+            Debug.Log(gm.timeInOven);
             //part 2
             //deciding level of cookness
-            if (gm.timeInOven >= gm.cookTime - 1 && gm.timeInOven <= gm.cookTime + 2)
+            if (gm.timeInOven >= gm.cookTime - 3 && gm.timeInOven <= gm.cookTime)
             {
                 //cake becomes darker, the vanilla and lemon cake become caramel colored and the chocolate becomes dark brown
-
-                if (gm.cake.GetComponent<MeshRenderer>().material.name != "Chocolate")
+                if (gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.name != "chocolate")
                 {
-                    //for (int i = 1; i < allChildren.Length; i++)
-                    //{
-                    //    allChildren[i].gameObject.GetComponent<Renderer>().material = Color.Lerp(allChildren[i].gameObject.GetComponent<MeshRenderer>().material.color, player.caramel.color, Time.deltaTime / 10);
-                    //}
-                    gm.cake.GetComponent<MeshRenderer>().material.color = Color.Lerp(gameObject.GetComponent<MeshRenderer>().material.color, player.caramel.color, Time.deltaTime / 10);
+                    
+                    gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.Lerp(gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.color, player.caramel.color, Time.deltaTime / 40); //player.caramel.color
+                    gm.cake.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.Lerp(gm.cake.transform.GetChild(1).GetComponent<MeshRenderer>().material.color, player.caramel.color, Time.deltaTime / 40);
                 }
                 else
                 {
-                    gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(gameObject.GetComponent<MeshRenderer>().material.color, player.darkBrown.color, Time.deltaTime / 10);
+                    gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.Lerp(gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.color, player.darkBrown.color, Time.deltaTime / 40);
+                    gm.cake.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.Lerp(gm.cake.transform.GetChild(1).GetComponent<MeshRenderer>().material.color, player.darkBrown.color, Time.deltaTime / 40);
                 }
             }
 
             else if (gm.timeInOven >= gm.cookTime + 2)
             {
+                Debug.Log("burning"); 
                 // all three cakes become black as time goes on
-                gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(gameObject.GetComponent<MeshRenderer>().material.color, Color.black, Time.deltaTime / 10);
+                gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.Lerp(gm.cake.transform.GetChild(0).GetComponent<MeshRenderer>().material.color, Color.black, Time.deltaTime / 40);
+                gm.cake.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.Lerp(gm.cake.transform.GetChild(1).GetComponent<MeshRenderer>().material.color, Color.black, Time.deltaTime / 40);
             }
             else if (gm.timeInOven >= gm.cookTime + 4)
             {
                 // the cake lights on fire
             }
             
+        } else
+        {
+            gm.timeInOven = 0f;
         }
     }
 }
